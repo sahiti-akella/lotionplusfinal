91ed9589762520f0cffc7d23faae5ac808cb428b
Sahitiakella <sahiti.akella@gmail.com>
2023-03-17 12:15:48 -0600

    Added Previous Lotion Code to src

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Layout from "./Layout";
+import WriteBox from "./WriteBox";
+import Empty from "./Empty";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Empty />} />
+          <Route path="/notes" element={<Empty />} />
+          <Route
+            path="/notes/:noteId/edit"
+            element={<WriteBox edit={true} />}
+          />
+          <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
+          {/* any other path */}
+          <Route path="*" element={<Empty />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
