da570daee14d57e8d57e6f529965880e0c4c4ec9
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-19 14:34:28 -0600

    wrapped application with GoogleOAuthProvider and started app.js

--- src/index.js
+++ src/index.js
@@ -6,25 +6,29 @@ import Layout from "./Layout";
 import WriteBox from "./WriteBox";
 import Empty from "./Empty";
 import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Empty />} />
-          <Route path="/notes" element={<Empty />} />
-          <Route
-            path="/notes/:noteId/edit"
-            element={<WriteBox edit={true} />}
-          />
-          <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
-          {/* any other path */}
-          <Route path="*" element={<Empty />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="243770027260-lie52p3h90tqff80regn7c88av460kbv.apps.googleusercontent.com">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Empty />} />
+            <Route path="/notes" element={<Empty />} />
+            <Route
+              path="/notes/:noteId/edit"
+              element={<WriteBox edit={true} />}
+            />
+            <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
+            {/* any other path */}
+            <Route path="*" element={<Empty />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </>
 );
 
