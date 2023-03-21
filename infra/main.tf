df453664fa99149ce64ab19947ee4f41b4bb732c
Sahitiakella <sahiti.akella@gmail.com>
2023-03-21 12:13:57 -0600

    added main.tf

---
+++ infra/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 4.0.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# specify the provider region
+provider "aws" {
+  region = "ca-central-1"
+}
