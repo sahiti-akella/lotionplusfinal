a7f6f55859bed3f5682dba30eabd8cb5738160cd
Hareem Khan <hareem.khan@ucalgary.ca>
2023-03-23 20:40:20 -0600

    fixed routing issue with react.strictmode

--- src/index.js
+++ src/index.js
@@ -12,28 +12,26 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <React.StrictMode>
-      <GoogleOAuthProvider clientId="243770027260-lie52p3h90tqff80regn7c88av460kbv.apps.googleusercontent.com">  
-       
-        <BrowserRouter>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Empty />} />
-              <Route path="/notes" element={<Empty />} />
-              <Route
-                path="/notes/:noteId/edit"
-                element={<WriteBox edit={true} />}
-              />
-              <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
-              {/* any other path */}
-              <Route path="*" element={<Empty />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        <App />
+    <GoogleOAuthProvider clientId="243770027260-lie52p3h90tqff80regn7c88av460kbv.apps.googleusercontent.com">  
+
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Empty />} />
+            <Route path="/notes" element={<Empty />} />
+            <Route
+              path="/notes/:noteId/edit"
+              element={<WriteBox edit={true} />}
+            />
+            <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
+            {/* any other path */}
+            <Route path="*" element={<Empty />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <App />
         
-      </GoogleOAuthProvider>
-    </React.StrictMode>
+    </GoogleOAuthProvider>
   </>
 );
 
