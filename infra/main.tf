c350de6f0f8c9a630d1c0af5480658d3f53e1345
Sahitiakella <sahiti.akella@gmail.com>
2023-03-24 19:19:28 -0600

    Added final main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,145 +1,296 @@
 terraform {
-  required_providers {
-    aws = {
-      version = ">= 4.0.0"
-      source  = "hashicorp/aws"
-    }
-  }
+ required_providers {
+   aws = {
+     version = ">= 4.0.0"
+     source  = "hashicorp/aws"
+   }
+ }
 }
+
 
 # specify the provider region
 provider "aws" {
-  region = "ca-central-1"
+ region = "ca-central-1"
 }
 
-# the locals block is used to declare constants that 
-# you can use throughout your code
-locals {
-  function_name = "save-note"
-  handler_name  = "main.lambda_handler"
-  artifact_name = "artifact.zip"
+
+#Delete_Note
+resource "aws_iam_role" "lambda_delete_note" {
+ name               = "iam-for-lambda-delete-note"
+ assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = "sts:AssumeRole"
+       Effect = "Allow"
+       Principal = {
+         Service = "lambda.amazonaws.com"
+       }
+     }
+   ]
+ })
 }
 
-# create a role for the Lambda function to assume
-# every service on AWS that wants to call other AWS services should first assume a role and
-# then any policy attached to the role will give permissions
-# to the service so it can interact with other AWS services
-# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
-resource "aws_iam_role" "lambda" {
-  name               = "iam-for-lambda-${local.function_name}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+
+data "archive_file" "delete-note-30140959-archive" {
+ type = "zip"
+ source_file = "../functions/delete-note/main.py"
+ output_path = "delete_note.zip"
 }
 
-# create archive file from main.py
-data "archive_file" "lambda" {
-  type = "zip"
-  # this file (main.py) needs to exist in the same folder as this 
-  # Terraform configuration file
-  source_file = "main.py"
-  output_path = "artifact.zip"
+
+resource "aws_lambda_function" "lambda_delete_note" {
+ role             = aws_iam_role.lambda_delete_note.arn
+ function_name    = "delete-note-30140959"
+ handler          = "main.lambda_handler"
+ filename         = "delete_note.zip"
+ source_code_hash = data.archive_file.delete-note-30140959-archive.output_base64sha256
+ runtime          = "python3.9"
 }
 
-# create a Lambda function
-# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
-resource "aws_lambda_function" "lambda" {
-  role             = aws_iam_role.lambda.arn
-  function_name    = local.function_name
-  handler          = local.handler_name
-  filename         = local.artifact_name
-  source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
-  runtime = "python3.9"
+resource "aws_iam_policy" "dynamodb_delete_policy" {
+ name = "dynamodb-delete-policy"
+
+
+ policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = "dynamodb:DeleteItem"
+       Effect = "Allow"
+       Resource = aws_dynamodb_table.lotion-30143555.arn
+     },
+     {
+       Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+       Effect = "Allow"
+       Resource = "arn:aws:logs:*:*:*"
+     },
+
+
+   ]
+ })
 }
 
-# create a policy for publishing logs to CloudWatch
-# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
-resource "aws_iam_policy" "logs" {
-  name        = "lambda-logging-${local.function_name}"
-  description = "IAM policy for logging from a lambda"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_delete_policy" {
+ policy_arn = aws_iam_policy.dynamodb_delete_policy.arn
+ role       = aws_iam_role.lambda_delete_note.name
 }
 
-# attach the above policy to the function role
-# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda.name
-  policy_arn = aws_iam_policy.logs.arn
+
+resource "aws_lambda_function_url" "delete_note_url" {
+ function_name      = aws_lambda_function.lambda_delete_note.function_name
+ authorization_type = "NONE"
+
+
+ cors {
+   allow_credentials = true
+   allow_origins     = ["*"]
+   allow_methods     = ["DELETE"]
+   allow_headers     = ["*"]
+   expose_headers    = ["keep-alive", "date"]
+ }
 }
 
-# create a Function URL for Lambda 
-# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
-resource "aws_lambda_function_url" "url" {
-  function_name      = aws_lambda_function.lambda.function_name
-  authorization_type = "NONE"
 
-  cors {
-    allow_credentials = true
-    allow_origins     = ["*"]
-    allow_methods     = ["GET", "POST", "PUT", "DELETE"]
-    allow_headers     = ["*"]
-    expose_headers    = ["keep-alive", "date"]
-  }
+output "delete_note_url" {
+ value = aws_lambda_function_url.delete_note_url.function_url
 }
 
-# show the Function URL after creation
-output "lambda_url" {
-  value = aws_lambda_function_url.url.function_url
+
+#Get_Note
+
+
+resource "aws_iam_role" "lambda_get_notes" {
+ name               = "iam-for-lambda-get-notes"
+ assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = "sts:AssumeRole"
+       Effect = "Allow"
+       Principal = {
+         Service = "lambda.amazonaws.com"
+       }
+     }
+   ]
+ })
 }
-view raw
-
-# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
-resource "aws_dynamodb_table" "lotion-30140959" {
-  name         = "lotion-30140959"
-  billing_mode = "PROVISIONED"
-
-  # up to 8KB read per second (eventually consistent)
-  read_capacity = 1
-
-  # up to 1KB per second
-  write_capacity = 1
 
 
-  hash_key = "email"
-  range_key = "id"
 
-  attribute {
-    name = "email"
-    type = "S"
-  }
-  attribute {
-    name = "id"
-    type = "S"
-  }
 
+data "archive_file" "get-notes-30140959-archive" {
+ type = "zip"
+ source_file = "../functions/get-notes/main.py"
+ output_path = "get_notes.zip"
+}
+
+
+resource "aws_lambda_function" "lambda_get_notes" {
+ role             = aws_iam_role.lambda_get_notes.arn
+ function_name    = "get-notes-30140959"
+ handler          = "main.lambda_handler"
+ filename         = "get_notes.zip"
+ source_code_hash = data.archive_file.get-notes-30140959-archive.output_base64sha256
+ runtime          = "python3.9"
+}
+
+
+resource "aws_iam_policy" "dynamodb_get_policy" {
+ name = "dynamodb-get-policy"
+
+
+ policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+      {
+       Action = ["dynamodb:*", "logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+       Effect = "Allow"
+       Resource = aws_dynamodb_table.lotion-30143555.arn
+     }
+   ]
+ })
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_get_policy" {
+ policy_arn = aws_iam_policy.dynamodb_get_policy.arn
+ role       = aws_iam_role.lambda_get_notes.name
+}
+
+
+resource "aws_lambda_function_url" "get_notes_url" {
+ function_name      = aws_lambda_function.lambda_get_notes.function_name
+ authorization_type = "NONE"
+
+
+ cors {
+   allow_credentials = true
+   allow_origins     = ["*"]
+   allow_methods     = ["GET"]
+   allow_headers     = ["*"]
+   expose_headers    = ["keep-alive", "date"]
+ }
+}
+
+
+output "get_notes_url" {
+ value = aws_lambda_function_url.get_notes_url.function_url
+}
+
+
+#Save_Note
+
+
+resource "aws_iam_role" "lambda_save_note" {
+ name               = "iam-for-lambda-save-note"
+ assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = "sts:AssumeRole"
+       Effect = "Allow"
+       Principal = {
+         Service = "lambda.amazonaws.com"
+       }
+     }
+   ]
+ })
+}
+
+
+data "archive_file" "save-note-30140959-archive" {
+ type = "zip"
+ source_file = "../functions/save-note/main.py"
+ output_path = "save_note.zip"
+}
+
+
+resource "aws_lambda_function" "lambda_save_note" {
+ role             = aws_iam_role.lambda_save_note.arn
+ function_name    = "save-note-30140959"
+ handler          = "main.lambda_handler"
+ filename         = "save_note.zip"
+ source_code_hash = data.archive_file.save-note-30140959-archive.output_base64sha256
+
+
+ runtime = "python3.9"
+}
+
+
+resource "aws_iam_policy" "dynamodb_save_policy" {
+ name = "dynamodb-save-policy"
+
+
+ policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = ["dynamodb:*", "logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+       Effect = "Allow"
+       Resource = aws_dynamodb_table.lotion-30143555.arn
+     }
+   ]
+ })
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_save_policy" {
+ role       = aws_iam_role.lambda_save_note.name
+ policy_arn = aws_iam_policy.dynamodb_save_policy.arn
+}
+
+
+resource "aws_lambda_function_url" "save_note_url" {
+ function_name      = aws_lambda_function.lambda_save_note.function_name
+ authorization_type = "NONE"
+
+
+ cors {
+   allow_credentials = true
+   allow_origins     = ["*"]
+   allow_methods     = ["POST"]
+   allow_headers     = ["*"]
+   expose_headers    = ["keep-alive", "date"]
+ }
+}
+
+
+output "save_note_url" {
+ value = aws_lambda_function_url.save_note_url.function_url
+}
+
+
+resource "aws_dynamodb_table" "lotion-30143555" {
+ name         = "lotion-30143555"
+ billing_mode = "PROVISIONED"
+
+
+ # up to 8KB read per second (eventually consistent)
+ read_capacity = 1
+
+
+ # up to 1KB per second
+ write_capacity = 1
+
+
+ # we only need a student id to find an item in the table; therefore, we
+ # don't need a sort key here
+ hash_key = "email"
+ range_key = "id"
+
+
+ # the hash_key data type is string
+ attribute {
+   name = "email"
+   type = "S"
+ }
+
+
+ attribute {
+   name = "id"
+   type = "S"
+ }
 }
